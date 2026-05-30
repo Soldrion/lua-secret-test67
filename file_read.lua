22d6af762fb45b60d21c7c3d3d28534fb93288ce
Soldrion <edinmemes@gmail.com>
2026-05-29 23:37:03 -0400

    changed back to lua cuz the luau interpreter is lowk tarded asf

--- file_read.lua
+++ file_read.lua
@@ -2,10 +2,11 @@ local file = io.open("architecture.md","r")
 local count = 10
 
 if file ~= nil then
-    while count >= 0 do
-        print(file:read("*line"))
-        if print(file:read("*line")) == nil then
+    while count > 0 do
+        print(file:read("*line"),"\n")
+        if file:read("*line") == nil then
             count =- 1
+            print"\n"
         else
             count = 10
         end
