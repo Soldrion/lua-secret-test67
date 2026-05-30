ffafbfbd9e0d2d9274319a29cd8d8e02bd0331a1
Soldrion <edinmemes@gmail.com>
2026-05-30 00:05:51 -0400

    config

--- main.lua
+++ main.lua
@@ -1,10 +1,23 @@
 local mod =  require("test")
+_G.love = require("love")
 
+
+function love.load()
+    _G.number = 0
+end
+
+function love.update(dt)
+    number = number +1
+end
+
+function love.draw()
+    love.graphics.print(number)
+end
 
 function main()
     print(mod.sum(10,5))
     mod.sayHello("Sigma")
-
+    
     return 0
 end
 
